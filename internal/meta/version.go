da28d2712136ed19c0b518fd53d521a8e9897837
HT4w5 <ht4w5x@gmail.com>
2026-01-29 21:21:19 +0800

    feat(meta): add version info

---
+++ internal/meta/version.go
@@ -0,0 +1,24 @@
+package meta
+
+import "fmt"
+
+var (
+	Name       = "song"
+	BuildDate  string
+	CommitHash string
+	Version    string
+	Platform   string
+	GoVersion  string
+)
+
+func VersionMultiline() string {
+	return fmt.Sprintf(
+		"%s %s (%s %s)\nBuild date: %s\nCommit hash: %s\n",
+		Name,
+		Version,
+		GoVersion,
+		Platform,
+		BuildDate,
+		CommitHash,
+	)
+}
